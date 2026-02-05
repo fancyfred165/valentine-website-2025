9fa81527d6dc5ab37ab2f3bb4a2a20360d491a84
fancyfred165 <fancyfred165@gmail.com>
2026-02-05 21:51:53 +1100

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dv0zozto8/video/upload/v1769948855/The_Temper_Trap_-_Sweet_Disposition_OFFICIAL_VIDEO_lsgonu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
